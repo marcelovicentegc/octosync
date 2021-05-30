10b3b0bac2e4a6265e20d0fbfae36341b488358e
Marcelo Cardoso <marcelovicentegc@gmail.com>
2021-05-30 11:33:26 -0300

    fix(clients): incorrect import

--- packages/clients/src/setup.ts
+++ packages/clients/src/setup.ts
@@ -1,14 +1,10 @@
 import { Github } from "./github";
-import { useEnv } from "./hooks";
+import { useEnv } from "@octosync/utils";
 import { Jira } from "./jira";
 
 function setup() {
-  const {
-    JIRA_HOST,
-    JIRA_PROJECT,
-    JIRA_PROJECT_ID,
-    GITHUB_ORGANIZATION,
-  } = useEnv();
+  const { JIRA_HOST, JIRA_PROJECT, JIRA_PROJECT_ID, GITHUB_ORGANIZATION } =
+    useEnv();
 
   const jira = new Jira(JIRA_HOST, JIRA_PROJECT, JIRA_PROJECT_ID);
   const github = new Github(GITHUB_ORGANIZATION);
